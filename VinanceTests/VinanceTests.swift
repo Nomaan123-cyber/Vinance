9e9076e0f6227b157fed2ed32dc2863b89680443
Nomaan Sayed <81969059+Nomaan123-cyber@users.noreply.github.com>
2025-06-25 17:47:34 +0100

    Add files via upload

---
+++ VinanceTests/VinanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  VinanceTests.swift
+//  VinanceTests
+//
+//  Created by Nomaan Sayed on 21/05/2025.
+//
+
+import Testing
+@testable import Vinance
+
+struct VinanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
